5d915df949779813ef9bfc3c83a47e197f0f5fef
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-03-24 20:56:29 +0100

    Holy shit de funkar, JÄMFÖR DEM OLIKA 2 KODERNA FRÅN FÖRRA PUSHEN, tack jesus. Ska göra 3x3 check ruta efter denna push

--- functions.cpp
+++ functions.cpp
@@ -6,11 +6,7 @@
 // 2. Inkorporera den i initializeSudokuMap
 // 3. Blir functionen falsk, kör om loop tills den går
 
-bool placementChecker(){
-    
 
-    true;
-}
 
 
 
@@ -19,7 +15,6 @@ bool placementChecker(){
 void initializeSudokuMap(){
 
 
-    
     std::vector<std::vector<int>> sudokuMap(9, std::vector<int>(9, 0)); // 9x9 grid, filled with 0s
 
     // Loops through first row
@@ -28,25 +23,22 @@ void initializeSudokuMap(){
         for (int j = 0; j < 9; j++)
         {
             //If cleanRow true = valid row with unique numbers
-            bool cleanRow = false;
-            bool cleanColumn = false;
+            bool validNumber = false;
             int attempts = 0;
 
             //While loop to check until the row is valid
-            while(!cleanRow && !cleanColumn)
+            while(!validNumber)
             {  
                 //Randomize number
                 int ranNum = (rand() % 9)+1;
 
-                //Assume row / column is clean
-                cleanRow = true;
-                cleanColumn = true;
+                //Assume row / column is valid
+                bool valid = true;
 
-                    //If a row number collides rerandomize number and repeat
                     for (int x = 0; x < j; x++)
                     {
                         if(ranNum == sudokuMap[i][x]){
-                            cleanRow = false; 
+                            valid = false; 
                             break;
                         }
                     }
@@ -54,20 +46,25 @@ void initializeSudokuMap(){
                     for (int y = 0; y < i; y++)
                     {
                         if(ranNum == sudokuMap[y][j]){
-                            cleanColumn = false; 
+                            valid = false; 
                             break;
                         }
                     }
 
 
-                    if(cleanRow && cleanColumn){
+                    if(valid){
                         sudokuMap[i][j] = ranNum;
+                        valid = true;
+                        break;
                     }
 
                     attempts++;
-                    if(attempts > 1000){
-                        std::cout << "Too many attempts were made";
-                        return;
+                    if(attempts > 100){
+                        for (int x = 0; x < 9; x++){
+                            sudokuMap[i][x] = 0;
+                        } 
+                        j = -1;
+                        break;
                     }
             }
         }
